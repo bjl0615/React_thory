a5feac03f16d9e173ee766bd8a9b0651a00b0f82
bjl0615 <bjl0615@gmail.com>
2021-09-13 20:25:34 +0900

    What_React.js

---
+++ React_Chapter/What_React.js
@@ -0,0 +1,24 @@
+/*
+React는 무엇인가?
+    A JavaScript library fot building user interface
+    (화면을 만들기 위한 자바스크립트 라이브러리)
+        라이브러리?
+            자주 쓰는 기능들을 모아놓은 것.
+
+    React의 장점
+        빠른 업데이트 & 렌더링 속도
+        Virtual(가상) DOM(Document Object Model)
+            우리가 흔히 보는 웹페이지를 정의하는 하나의 객체
+        Component-Based(구성요소 기반)
+            레고 블록을 조립하듯이 컴포넌트를 모아서 개발
+        Reusability(재사용성)
+            개발 속도가 빨라짐
+        React Native까지 배우면 모바일앱도 개발 가능
+
+    React의 단점
+        공부를 해야 함...
+            JSX? Component? State? Proprs?
+        계속 뭔가 바뀜...
+            
+
+*/
