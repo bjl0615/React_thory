9443005a29f8dde9d6ff178fb0a8205942bdff53
bjl0615 <bjl0615@gmail.com>
2021-09-13 23:18:46 +0900

    ADD index

--- React_Chapter/my-app/src/index.js
+++ React_Chapter/my-app/src/index.js
@@ -4,22 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const formatName = function (name) {
-  return name.firstName + " " + name.lastName;
+// ReactDOM.render(<App />, document.getElementsById('root'));
+
+function tick(){
+  const element = (
+    <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+
+  ReactDOM.render(
+    element,
+    document.getElementById('root')
+  );
 }
 
-const name = { 
-  firstName : "Inje",
-  lastName : "Lee",
-};
-
-const element = <h1>Hello, {formatName(name)}!</h1>
-
-
-ReactDOM.render(
-  element,
-  document.getElementById('root')
-);
+setInterval(tick,1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
