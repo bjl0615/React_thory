23057da347421ef5805d44e8fc96ce3009857be2
bjl0615 <bjl0615@gmail.com>
2021-09-15 00:25:35 +0900

    Add React

--- React_Chapter/my-app/src/App.js
+++ React_Chapter/my-app/src/App.js
@@ -1,10 +1,19 @@
+import React from 'react';
+import Comment from './Comment';
 import logo from './logo.svg';
 import './App.css';
 
+const comments = [
+    { name:'Inje Lee', content:'My comment!' },
+    { name:'현빈', content:'이게 최선입니까? 확실해요?'},
+    { name:'Tony Stark', content:'I am Iron man'}
+];
+
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ padding: 16, backgroundColor: '#282c34' }}>
       <header className="App-header">
+        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +27,16 @@ function App() {
           Learn React
         </a>
       </header>
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+          {comments.map((comment, index) => {
+              return (
+                  <Comment
+                      name={comment.name}
+                      content={comment.content}
+                  />
+              )
+          })}
+      </div>
     </div>
   );
 }
