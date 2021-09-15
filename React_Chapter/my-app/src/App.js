e87663da59b79954bc343c03c8c6c68ff6c933ee
bjl0615 <bjl0615@gmail.com>
2021-09-15 15:43:47 +0900

    ADD React

--- React_Chapter/my-app/src/App.js
+++ React_Chapter/my-app/src/App.js
@@ -3,15 +3,43 @@ import Comment from './Comment';
 import logo from './logo.svg';
 import './App.css';
 
-const comments = [
-    { name:'Inje Lee', content:'My comment!' },
-    { name:'현빈', content:'이게 최선입니까? 확실해요?'},
-    { name:'Tony Stark', content:'I am Iron man'}
+const commentFromServer = [
+  { name: 'Inje Lee', content: 'My comment!'},
+  { name: '현빈', content: '이게 최선입니까?'},
+  { name : 'Tony Stark', conetent: 'I am Iron Man. '}
 ];
 
-function App() {
-  return (
-    <div className="App" style={{ padding: 16, backgroundColor: '#282c34' }}>
+var timer;
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      comments : [],
+    };
+  }
+
+  componentDidMount(){
+    let comments = this.state.comments;
+    timer = setInterval(() => {
+      if(comments.length < commentFromServer.length){
+        let index = comments.length;
+        comments.push(commentFromServer[index]);
+        this.setState({
+          comments: comments
+        });
+      }else if(timer){
+        clearInterval(timer);
+      }
+    }, 1000);
+  }
+  render(){
+    const { comments } = this.state;
+
+    return (
+      <div className="App" style={{ padding: 16, backgroundColor: '#282c34' }}>
       <header className="App-header">
         
         <img src={logo} className="App-logo" alt="logo" />
@@ -23,22 +51,22 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
+          >
           Learn React
         </a>
       </header>
-      <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <div>         
           {comments.map((comment, index) => {
-              return (
-                  <Comment
-                      name={comment.name}
-                      content={comment.content}
-                  />
+            return (
+              <Comment
+              name={comment.name}
+              content={comment.content}
+              />
               )
-          })}
+            })}
       </div>
     </div>
   );
 }
-
+}
 export default App;
