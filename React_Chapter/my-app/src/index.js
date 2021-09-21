6f69c267301edecf80b20caeed3000c1be3ea4f4
bjl0615 <bjl0615@gmail.com>
2021-09-21 16:29:00 +0900

    Add React

--- React_Chapter/my-app/src/index.js
+++ React_Chapter/my-app/src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Calculator from './Chapter10/Calculator'
-import Greeting from './Chapter07/Greeting';
+import UserProfileList from './Chapter11/UserProfileList'
 
 
-ReactDOM.render(<Calculator />, document.getElementById('root'));
+ReactDOM.render(<UserProfileList />, document.getElementById('root'));
